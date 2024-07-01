6a430745f1068ea96654331fb7eb4d393bfa1c1f
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-07-01 13:43:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 class Config:
-    API_ID = ""
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = "28243586"
+    API_HASH = "4022d5686b9b7a7cf8891205921a0ab3"
+    BOT_TOKEN = "4022d5686b9b7a7cf8891205921a0ab3"
     BOT_SESSION = "" 
-    DATABASE_URI = ""
-    DATABASE_NAME = ""
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
-    BOT_OWNER_ID = [int(id) for id in os.environ.get("BOT_OWNER_ID", '').split()]
+    DATABASE_URI = "mongodb+srv://madarazbotz:Olm2V88uGXRS5m8y@cluster0.i58ntas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DATABASE_NAME = "Cluster0"
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5340652544').split()]
+    BOT_OWNER_ID = [int(id) for id in os.environ.get("BOT_OWNER_ID", '5340652544').split()]
     UPDATES_CHANNEL = ""
 
 class temp(object): 
