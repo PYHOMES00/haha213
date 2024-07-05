6bc91661e4463d2d18d8140a9566fe0264a2372e
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-05 14:16:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = "28243586"
     API_HASH = "4022d5686b9b7a7cf8891205921a0ab3"
     BOT_TOKEN = "4022d5686b9b7a7cf8891205921a0ab3"
-    BOT_SESSION = "" 
+    BOT_SESSION = "forward-bot" 
     DATABASE_URI = "mongodb+srv://madarazbotz:Olm2V88uGXRS5m8y@cluster0.i58ntas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DATABASE_NAME = "Cluster0"
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5340652544').split()]
