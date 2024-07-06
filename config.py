0d36c96525befd1b0cb6359c15bf5786c85abf43
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-07-06 09:53:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 class Config:
     API_ID = "28243586"
     API_HASH = "4022d5686b9b7a7cf8891205921a0ab3"
-    BOT_TOKEN = "7151356654:AAF5JV1bK-CzeB_KlAUKABipL_JjIjFwEzA"
+    BOT_TOKEN = "6547738956:AAFaJBopf5Utz8udPSTidNsGoOZEDQdOzhU"
     BOT_SESSION = "forward-bot" 
-    DATABASE_URI = "mongodb+srv://madarazbotz:Olm2V88uGXRS5m8y@cluster0.i58ntas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DATABASE_URI = "mongodb+srv://madarazbotz:O8WtNAEReh6ohJEt@cluster0.9mosuuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DATABASE_NAME = "Cluster0"
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5340652544').split()]
     BOT_OWNER_ID = [int(id) for id in os.environ.get("BOT_OWNER_ID", '5340652544').split()]
-    UPDATES_CHANNEL = "-1002173648219"
+    UPDATES_CHANNEL = ""
 
 class temp(object): 
     lock = {}
